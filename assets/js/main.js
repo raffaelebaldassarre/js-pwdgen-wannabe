9a60cb7c85ba14f7d6a421d5e6e6ab0a6a8bc529
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-11-12 15:08:55 +0100

    Inserito file js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var name = prompt ("Come ti chiami?");
+
+alert("Ciao " + name);
+
+var age = prompt ("Quanti anni hai?");
+var color = prompt ("Qual è il tuo colore preferito?")
+
+document.getElementById("result").innerHTML = name + age + color + "20";
